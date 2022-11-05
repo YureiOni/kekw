56cbe0a2a08d78ffdc3eeb2bb3f3871dac555158
GIANLUCA BRUZAO <gianluca.bruzao@senacsp.edu.br>
2022-11-04 22:16:09 -0300

    Add project files.

---
+++ kekw/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kekw.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Destricao { get; set; }
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ kekw/Models/Lanche.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kekw.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string  Nome { get; set; }
+        public string  DestricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string  ImagemThumbnail { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool Estoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
